3f811590be21d2b17a4c3c0014f0184b31323738
Yuto Urashima <yuto.urashima24@gmail.com>
2023-11-08 21:26:06 +0900

    Fix RevenueChar and LatestInvoices component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,12 @@
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
 
 export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices()
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -17,8 +23,8 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   )
